09f9cdf0a7bc6d44a3189651f50ed4a1260fa261
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-05 13:55:59 +0200

    final exercise of the chapter on dates

---
+++ 03-dates/04-locale-date-Optional/script.js
@@ -0,0 +1,25 @@
+/* becode/javascript
+ *
+ * /04-dates/04-locale-date/script.js - 4.4: date textuelle
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+
+    // your code here
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    // alert(today)
+    document.getElementById("target").innerText = today;
+})();
