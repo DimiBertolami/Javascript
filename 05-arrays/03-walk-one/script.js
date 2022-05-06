aa4f1c6a38a5b24dbe91ef21754ab6f08a45fbfa
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-06 15:08:55 +0200

    iterating through the array

---
+++ 05-arrays/03-walk-one/script.js
@@ -0,0 +1,34 @@
+/* becode/javascript
+ *
+ * /05-arrays/01-get-element/script.js - 5.1: élément de tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var fruits = [
+        "apple",
+        "pear",
+        "raspberry",
+        "tomatos",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peache",
+        "grap",
+        "cherry",
+    ];
+
+    // end of array is array.length-1
+    for (let i = 0; i < fruits.length-1; i++) {
+        // alert(fruits[i]);
+        console.log(fruits[i]);
+    }
+})();
