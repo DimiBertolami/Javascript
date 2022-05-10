40f0de7bd9360bcfaee593195cbcd698e5996b67
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-10 09:53:07 +0200

    i actually tried this while struggling in previous exercise

---
+++ 06-dom/09-match-password-two-Optional/script.js
@@ -0,0 +1,24 @@
+/* becode/javascript
+ *
+ * /06-dom/10-match-password-two/script.js - 6.10: vérification de mots de passe (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        let pOne = document.getElementById("pass-one");
+        let pTwo = document.getElementById("pass-two");
+
+        if (pOne.value !== pTwo.value){
+            pOne.className = "error";
+            pTwo.className = "error";
+        }
+    })
+})();
