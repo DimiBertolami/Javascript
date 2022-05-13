1386c304625380c1c391654e52352ea03496e34a
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-13 10:55:30 +0200

    branching changes to keep original code unharmed background color purple i want emoji's to be bigger in size but haven't figured that out yet Will now work a little on another project

--- timwatch/game.js
+++ timwatch/game.js
@@ -1,7 +1,28 @@
-
+// var canvas = document.getElementById('game');
+// var context = canvas.getContext('2d');
+//
 const player = document.getElementById("ReadyPlayerOne");
-let x = 300;
-let y= 300;
+var grid = 16;
+var count = 0;
+let x = innerWidth/2;
+let y= innerHeight/2;
+//
+// var snake = {
+//     x: 260,
+//     y: 160,
+//
+//     // snake velocity. moves one grid length every frame in either the x or y direction
+//     dx: grid,
+//     dy: 0,
+//
+//     // keep track of all grids the snake body occupies
+//     cells: [],
+//
+//     // length of the snake. grows when eating an apple
+//     maxCells: 4
+// };
+//
+//
 let direction;
 
 document.body.addEventListener("keydown", function(event){
@@ -10,12 +31,14 @@ document.body.addEventListener("keydown", function(event){
     }
 );
 document.body.addEventListener("keyup", function(event){
-    direction = event.key;
-    // console.log("letting go of key: " + direction);
+    // direction = event.key;
+    direction = "";
     }
 )
 
 function main(){
+    // requestAnimationFrame(main);
+
     if(direction === 'ArrowRight'){
         x++;
     }
@@ -32,11 +55,11 @@ function main(){
     setTimeout(main, 20);
 }
 
+        // requestAnimationFrame(main);
 main();
 
 function movePlayer(x, y){
     let moveXY = document.getElementById("ReadyPlayerOne");
-    console.log("x: " + x + " y: " + y);
     moveXY.style.left = x + "px";
     moveXY.style.top  = y + "px";
 }
