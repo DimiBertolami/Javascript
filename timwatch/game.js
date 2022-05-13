2a5796f970138936be1633cc8e85e89cddf65e43
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-13 12:47:45 +0200

    playerbox will now never cross any of the boundaries of the canvas element

--- timwatch/game.js
+++ timwatch/game.js
@@ -18,18 +18,19 @@ document.body.addEventListener("keyup", function(event){
 function main(){
     if(direction === 'ArrowRight'){
         x++;
+        x= x+10;
     }
     if(direction === 'ArrowLeft'){
-        x--;
+        x=x-10;
     }
     if(direction === 'ArrowDown'){
-        y++;
+        y= y+10;
     }
     if(direction === 'ArrowUp'){
-        y--;
+        y=y-10;
     }
     movePlayer(x, y);
-    setTimeout(main, 20);
+    setTimeout(main, 50);
 }
 
 main();
@@ -37,6 +38,22 @@ main();
 function movePlayer(x, y){
     let moveXY = document.getElementById("ReadyPlayerOne");
     console.log("x: " + x + " y: " + y);
+    if(x<=7){
+        x=7
+        direction = "";
+    }
+    if(y<=7){
+        y=7
+        direction = "";
+    }
+    if(x>=1336){
+        x=1336
+        direction = "";
+    }
+    if(y>=588){
+        y=588
+        direction = "";
+    }
     moveXY.style.left = x + "px";
     moveXY.style.top  = y + "px";
 }
